99e1faff144162d2865002461a9fcb629f473c88
SUTIKSHAN MISHRA <sutikshan.mishra33@gmail.com>
2022-12-03 07:22:11 +0545

    COntact file edited

--- components/Contacts.js
+++ components/Contacts.js
@@ -181,7 +181,7 @@ const Contacts = () => {
                 </a>
                 {/* ::Address */}
                 <p className="text-base text-gray-500 leading-6">
-                  Near Basbitti , Dudhmati-23 <br /> JanakpurDham
+                  Near Basbitti , Dudhmati Bridge-23 <br /> JanakpurDham
                   <br /> Nepal
                 </p>
                 {/* ::Socials */}
